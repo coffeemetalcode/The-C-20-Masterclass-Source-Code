f6ac20e80868881adb915ac311dc73c3bccbc160
David B. Stocker <coffeemetalcode@gmail.com>
2024-09-21 05:53:27 -0400

    finish 4.6

--- 04.VariablesAndDatatypes/4.6Booleans/main.cpp
+++ 04.VariablesAndDatatypes/4.6Booleans/main.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 
+// jump back in https://youtu.be/8jLOx1hD3_o?t=15400
 
-int main(){
+int main()
+{
 
-    bool red_light {false};
-    bool green_light{true};
-    
-    if(red_light == true){
-        std::cout << "Stop!" << std::endl;
-    }else{
-        std::cout << "Go through!" << std::endl;
-    }
+  bool red_light{false};
+  bool green_light{true};
 
-    if(green_light){
-        std::cout << "The light is green!" << std::endl;
-    }else{
-        std::cout << "The light is NOT green!" << std::endl;
-    }
+  if (red_light == true)
+  {
+    std::cout << "Stop!" << std::endl;
+  }
+  else
+  {
+    std::cout << "Go through!" << std::endl;
+  }
 
-    //sizeof()
-    std::cout << "sizeof(bool) : " << sizeof(bool) << std::endl;
+  if (green_light)
+  {
+    std::cout << "The light is green!" << std::endl;
+  }
+  else
+  {
+    std::cout << "The light is NOT green!" << std::endl;
+  }
 
+  // sizeof()
+  std::cout << "sizeof(bool) : " << sizeof(bool) << std::endl;
 
-    //Printing out a bool
-    //1 -->> true
-    //0 -->> false
-    std::cout << std::endl;
-    std::cout << "red_light : " << red_light << std::endl;
-    std::cout << "green_light : " << green_light << std::endl;
+  // Printing out a bool
+  // 1 -->> true
+  // 0 -->> false
+  std::cout << std::endl;
+  std::cout << "red_light : " << red_light << std::endl;
+  std::cout << "green_light : " << green_light << std::endl;
 
-    std::cout << std::boolalpha;
-    std::cout << "red_light : " << red_light << std::endl;
-    std::cout << "green_light : " << green_light << std::endl;
-   
-    return 0;
+  std::cout << std::boolalpha;
+  std::cout << "red_light : " << red_light << std::endl;
+  std::cout << "green_light : " << green_light << std::endl;
+
+  return 0;
 }
